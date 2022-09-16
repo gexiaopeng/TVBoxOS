548fec3009127ac769bacbfbd1ea1476380e8da8
gexiaopeng <gexiaopeng@tom.com>
2022-09-16 16:06:53 +0800

    Update App.java

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -31,7 +31,7 @@ public class App extends MultiDexApplication {
         initParams();
         // OKGo
         OkGoHelper.init();
-        // 初始化Web服务器test88999
+        // 初始化Web服务器test889999988
         ControlManager.init(this);
         //初始化数据库
         AppDataManager.init();
