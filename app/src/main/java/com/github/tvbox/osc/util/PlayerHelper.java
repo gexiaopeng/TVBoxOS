b5dc25fe5d52f96848f7849b518212b97aa3089c
gexiaopeng <gexiaopeng@tom.com>
2022-09-20 11:35:09 +0800

    ok

--- app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
+++ app/src/main/java/com/github/tvbox/osc/util/PlayerHelper.java
@@ -176,9 +176,9 @@ public class PlayerHelper {
             playersExist.put(0, true);
             playersExist.put(1, true);
             playersExist.put(2, true);
-            playersExist.put(10, MXPlayer.getPackageInfo() != null);
-            playersExist.put(11, ReexPlayer.getPackageInfo() != null);
-            playersExist.put(12, Kodi.getPackageInfo() != null);
+            //playersExist.put(10, MXPlayer.getPackageInfo() != null);
+            //playersExist.put(11, ReexPlayer.getPackageInfo() != null);
+            //playersExist.put(12, Kodi.getPackageInfo() != null);
             mPlayersExistInfo = playersExist;
         }
         return mPlayersExistInfo;
