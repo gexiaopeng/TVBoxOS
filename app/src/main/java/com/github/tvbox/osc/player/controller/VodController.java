6c6f1b0cf8181288adc1de9de2d1797c09a90a56
gexiaopeng <gexiaopeng@tom.com>
2022-11-04 14:39:04 +0800

    ok

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -137,14 +137,8 @@ public class VodController extends BaseController {
                    }else if(width>1800){
                        rate=(int)(width*0.90);
                    }
-                   mHandler.post(new Runnable() {
-                       @Override
-                       public void run() {
-                           Toast.makeText(context,"is:"+isPortrait+"|"+width,Toast.LENGTH_LONG).show();
-                       }
-                   });
                    if(isPortrait){
-                       rate=(int)(rate*0.98);
+                       rate=(int)(rate*0.95);
                    }
                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(rate, LinearLayout.LayoutParams.MATCH_PARENT);
                    mTopRoot1.setLayoutParams(lp);
